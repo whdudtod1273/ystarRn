2f4f1bb0a2e578f10df76276143367fe898aba01
whdudtod1273 <whdudtod1273@naver.com>
2021-03-13 13:48:52 +0900

    Follow.js add

---
+++ src/screen/profile/Follow.js
@@ -0,0 +1,18 @@
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  Pressable,
+} from 'react-native';
+
+function Follow() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View></View>
+    </SafeAreaView>
+  );
+}
+export default Follow;
